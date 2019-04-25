29fec590451f4b7386081925c32c3219f2dccb03
helenasimmons <helenacctx@gmail.com>
2019-04-25 14:10:17 -0500

    yeet shows up

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
-import pygame
-import os, sys
-#from pygame.locals import *
+import pygame, sys
+from pygame.locals import *
 import itertools
-#from pygame.sprite import Sprite
+from pygame.sprite import Sprite
 
+# class Avatar(pygame.sprite.Sprite):
+#     def __init__(self, width, height):
+#         pygame.sprite.Sprite.__init__(self)
+
+#         self.image = pygame.Surface([width, height])
+#         self.image.fill('white')
+#         self.rect = self.image.get_rect()
 
 class Yeet(pygame.sprite.Sprite):
     def __init__(self, color = (0,0,0), width = 100, hieght = 100):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load("yeet.jpg")
+        self.image = pygame.image.load("yeet.png")
 
         self.rect = self.image.get_rect()
 
@@ -20,24 +26,26 @@ class Yeet(pygame.sprite.Sprite):
 class Game():
     def __init__(self):
         pygame.init()
-        img = pygame.image.load("blorenge.png")
-        #yee = pygame.image.load("yeet.jpg")
-        pygame.display.set_icon(img) #sets an icon for the window
+        self.Avatar = pygame.image.load("yeet.png")
+        self.img = pygame.image.load("blorenge.png")
+        pygame.display.set_icon(self.img) #sets an icon for the window
+        self.player = Yeet()
+        self.all_sprites = pygame.sprite.Group()
 
-        screen = pygame.display.set_mode((1000,1000))
+        self.screen = pygame.display.set_mode((1000,1000))
         pygame.display.set_caption("Yeet 'n' Skeet") #name for the window
-
-        player_1 = Yeet()
-
-
+        
         running = True
+    
+        
         while running:
-            screen.blit(img, (0,0)) #replace img with an image the size
+            
+            #self.screen.blit(self.Avatar, (500,500)) #replace img with an image the size
                                     #of the screen to serve as background
-            y.set_pos(0,0)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            #pygame.display.update() 
+            pygame.display.update() 
+
 Game()
