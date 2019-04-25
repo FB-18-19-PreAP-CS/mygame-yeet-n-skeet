fb07a943aabe45fbffc5ece9c54f9d2bb77eb6b0
KaylaMck202 <kaylamck202@gmail.com>
2019-04-25 13:52:45 -0500

    Got yeet to appear!

--- game.py
+++ game.py
@@ -6,29 +6,31 @@ from pygame.sprite import Sprite
 #class Game():
     # def __init__():
     #     pygame.init()
-class Avatar(pygame.sprite.Sprite):
-    def __init__(self, width, height):
-        pygame.sprite.Sprite.__init__(self)
+# class Avatar(pygame.sprite.Sprite):
+#     def __init__(self, width, height):
+#         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([width, height])
-        self.image.fill('white')
-
-        self.rect = self.image.get_rect()
+#         self.image = pygame.Surface([width, height])
+#         self.image.fill('white')
+#         self.rect = self.image.get_rect()
 class Game():
     def __init__(self):
         pygame.init()
-        img = pygame.image.load("blorenge.png")
-        pygame.display.set_icon(img) #sets an icon for the window
+        self.Avatar = pygame.image.load("yeet.png")
+        self.img = pygame.image.load("blorenge.png")
+        pygame.display.set_icon(self.img) #sets an icon for the window
 
-        screen = pygame.display.set_mode((1000,1000))
+        self.screen = pygame.display.set_mode((1000,1000))
         pygame.display.set_caption("Yeet 'n' Skeet") #name for the window
 
+        
+        
+        
         running = True
     
-        Avatar( 60,90)
         
         while running:
-            screen.blit(img, (0,0)) #replace img with an image the size
+            self.screen.blit(self.Avatar, (500,500)) #replace img with an image the size
                                     #of the screen to serve as background
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -37,6 +39,8 @@ class Game():
             pygame.display.update() 
 
 Game()
+
+# blit(avatar(x,y))
 
 
 # class yeet(Sprite):
