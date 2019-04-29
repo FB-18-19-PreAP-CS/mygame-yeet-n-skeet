ec02317c5c4f96db2c0462a5670a21592da83406
KaylaMck202 <kaylamck202@gmail.com>
2019-04-29 14:03:42 -0500

    work on movement of skeet

--- game.py
+++ game.py
@@ -18,6 +18,7 @@ class Game():
         pygame.init()
         self.Avatar1 = pygame.image.load("yeet.png")
         self.Skeet = pygame.image.load("skeet.png")
+        self.Dave = pygame.image.load('Dave .png')
         self.img = pygame.image.load("blorenge.png")
         pygame.display.set_icon(self.img) #sets an icon for the window
 
@@ -33,13 +34,46 @@ class Game():
         while running:
             self.screen.blit(self.img, (500,500)) #replace img with an image the size
                                     #of the screen to serve as background
-            self.screen.blit(self.Avatar1, (50, 870))
-            self.screen.blit(self.Skeet, (870, 870))
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+            self.screen.blit(self.Avatar1, (10, 870))
+            self.screen.blit(self.Skeet, (879, 870))
+            self.screen.blit(self.Dave, (450, 10))
+            # for event in pygame.event.get():
+            #     if event.type == pygame.QUIT:
+            #         running = False
 
             pygame.display.update() 
+        def skeet_left(self):
+            skeet_posx = 879
+            skeet_posy = 870
+            step_skeetx = 10
+            step_skeety = 10
+            if skeet_posx>screen_width-64 or skeet_posx<0:
+                step_skeetx = -step_skeetx
+            if skeet_posy>screen_height-64 or skeet_posy<0:
+                step_skeety = -step_skeety
+
+            skeet_posx += step_skeetx
+            skeet_posy += step_skeety
+            
+        # def start(self):
+        #     while self.running:
+        #         self.screen.blit(self.img, (500,500)) #replace img with an image the size of the screen to serve as background
+            
+            
+        #     #self.yeet_left()
+            
+        #     #self.screen.blit(self.yeet, (50, 870))
+
+        #     #self.screen.blit(self.skeet, (870, 870))
+        #         for event in pygame.event.get():
+        #             if event.type == pygame.QUIT:
+        #                 self.running = False
+
+        #             if event.type == pygame.KEYDOWN:
+        #                 if event.key == pygame.K_A:
+        #                     pass
+        #                 if event.key == pygame.K_D:
+        #                     self.skeet_right()
 
 Game()
 
