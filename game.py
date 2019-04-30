d14d44fc5db827a925a39405d87b1cff347e8f02
helenasimmons <43218024+helenasimmons@users.noreply.github.com>
2019-04-30 14:11:51 -0500

    began work on collisions

--- game.py
+++ game.py
@@ -10,8 +10,11 @@ yeet = pygame.image.load("yeet.png")
 yeet_x = 50
 yeet_y = 50
 move = 5 #how many pixels it moves
-
 run = True
+
+def border():
+    pass
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,8 +24,8 @@ while run:
         yeet_x += move
     if keys[pygame.K_LEFT]:
         yeet_x -= move
-    
-    screen.fill((255,255,255)) #replace with bg
+
+    screen.fill((0,0,0)) #replace with bg
     screen.blit(yeet, (yeet_x,yeet_y))
     pygame.display.update() 
 
@@ -37,55 +40,33 @@ while run:
 #         self.skeet = pygame.image.load("skeet.png")
 #         self.img = pygame.image.load("blorenge.png")
 #         pygame.display.set_icon(self.img) #sets an icon for the window
-
 #         self.screen = pygame.display.set_mode((1000,1000))
 #         pygame.display.set_caption("Yeet 'n' Skeet") #name for the window
-        
 #         self.screen.blit(self.yeet, (500,500))
-
 #         self.running = True
-
-
 #     def yeet_left(self):  #will change to cover both later. trying Yeet first
 #         yeet_pos = self.yeet.get_rect()
 #         self.screen.blit(self.yeet, (yeet_pos[0]-50, yeet_pos[1]))
-
 #     def yeet_right(self, yeet_pos):
 #         #yeet_pos = self.yeet.get_rect()
 #         self.screen.blit(self.yeet, (self.yeet_pos[0]+50, self.yeet_pos[1]))
-#         print(self.yeet.get_rect())
-        
-
+#         print(self.yeet.get_rect()
 #     def start(self):
 #         while self.running:
 #             self.screen.blit(self.img, (500,500)) #replace img with an image the size of the screen to serve as background
 #             #self.screen.blit(self.yeet, (50, 870))
-
 #             #self.screen.blit(self.skeet, (870, 870))
 #             for event in pygame.event.get():
 #                 if event.type == pygame.QUIT:
 #                     self.running = False
-
 #                 if event.type == pygame.KEYDOWN:
 #                     if event.key == pygame.K_LEFT:
 #                         #yeet_pos = self.yeet.get_rect()
 #                         self.yeet_left()
 #                     if event.key == pygame.K_RIGHT: 
 #                         yeet_pos = self.yeet.get_rect()
-                        
 #                         #print()
 #                         #self.yeet_right(self.yeet_pos)
 #             self.screen.blit(self.img, (500,500))
 #             #self.draw_yee()
 #             pygame.display.update() 
-
-
-    
-
-# def main():
-#     g = Game()
-#     #g.yeet_left()
-#     g.start()
-
-# if __name__ == "__main__":
-#     main()
