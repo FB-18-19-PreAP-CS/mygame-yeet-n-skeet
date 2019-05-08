73972e9642e896e0888f74fd81de4dadadd39564
helena simmons <helenacctx@gmail.com>
2019-05-08 14:06:47 -0500

    progress on platforms

--- game.py
+++ game.py
@@ -23,10 +23,11 @@ if num == 1:
     yeet_x = 10
     yeet_y = 905
     move = 5
-    Skeet_x =879
-    Skeet_y =905   
+    Skeet_x = 879
+    Skeet_y = 905   
+
             
-            
+
     running = True
         
             
@@ -37,6 +38,8 @@ if num == 1:
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 print(pygame.mouse.get_pos())
+                
+                
         keys = pygame.key.get_pressed()
         
         #temp yeet controls
@@ -48,8 +51,6 @@ if num == 1:
             Skeet_y += move
         if keys[pygame.K_w]:
             Skeet_y -= move
-        
-        
         
         if keys[pygame.K_RIGHT]:
             yeet_x +=move
@@ -95,14 +96,30 @@ if num == 1:
                 Skeet_y += move
             
         #middle
-        if yeet_x >= 406 and yeet_x <= 600:
+        if yeet_x >= 350 and yeet_x <= 580:
             if yeet_y <= 480 and yeet_y >= 435:
                 yeet_y -= move
         if yeet_x >= 400 and yeet_x <= 593:
             if yeet_y <= 580 and yeet_y >= 435:
                 yeet_y += move
-    
 
+        #middle two 
+        if yeet_x >= 135 and yeet_x <= 384:
+            if yeet_y >= 408 and yeet_y <= 420:
+                yeet_y += move
+        if yeet_x >= 135 and yeet_x <= 384:
+            if yeet_y >= 272 and yeet_y <= 275:
+                yeet_y -= move
+
+        if yeet_x >= 605 and yeet_x <= 840:
+            if yeet_y >= 400 and yeet_y <= 405:
+                yeet_y += move
+        if yeet_x >= 605 and yeet_x <= 840:
+            if yeet_y >= 283 and yeet_y <= 285:
+                yeet_y -= move
+
+
+        print(yeet_x, yeet_y)
 
         screen.blit(img, (1,1))
         screen.blit(Dave, (450,10))
@@ -183,89 +200,6 @@ elif num == 2:
         screen.blit(Skeet, (Skeet_x,Skeet_y))
         pygame.display.update()
 
-
-
-# pygame.init()
-# yeet = pygame.image.load("yeet.png")
-# Skeet = pygame.image.load("skeet.png")
-# Dave = pygame.image.load('Dave .png')
-# img = pygame.image.load("blorenge.png")
-# pygame.display.set_icon(img) #sets an icon for the window
-
-# screen = pygame.display.set_mode((1000,1000))
-# pygame.display.set_caption("Yeet 'n' Skeet") #name for the window
-
-# yeet_x = 10
-# yeet_y = 905
-# move = 5
-# Skeet_x =879
-# Skeet_y =905   
-        
-        
-# running = True
-    
-        
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#     keys = pygame.key.get_pressed()
-#     if keys[pygame.K_RIGHT]:
-#         yeet_x +=move
-#     if keys[pygame.K_LEFT]:
-#         yeet_x -= move
-#     if keys[pygame.K_a]:
-#         Skeet_x -= move
-#     if keys[pygame.K_d]:
-#         Skeet_x += move
-#     screen.blit(img, (1,1))
-#     screen.blit(Dave, (450,10))
-#     screen.blit(yeet, (yeet_x,yeet_y))
-#     screen.blit(Skeet, (Skeet_x,Skeet_y))
-#     pygame.display.update()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Game():
-#     def __init__(self):
-#         pygame.init()
-#         self.yeet = pygame.image.load("yeet.png")
-#         self.Skeet = pygame.image.load("skeet.png")
-#         self.Dave = pygame.image.load('Dave .png')
-#         self.img = pygame.image.load("blorenge.png")
-#         pygame.display.set_icon(self.img) #sets an icon for the window
-
-#         self.screen = pygame.display.set_mode((1000,1000))
-#         pygame.display.set_caption("Yeet 'n' Skeet") #name for the window
-
-        
-        
-        
-#         running = True
-    
-        
-#         while running:
-#             self.screen.blit(self.img, (500,500)) #replace img with an image the size
-#                                     #of the screen to serve as background
-#             self.screen.blit(self.yeet, (10, 870))
-#             self.screen.blit(self.Skeet, (879, 870))
-#             self.screen.blit(self.Dave, (450, 10))
-#             # for event in pygame.event.get():
-#             #     if event.type == pygame.QUIT:
-#             #         running = False
-
-#             pygame.display.update() 
 
 
     # def skeet_left(self):
